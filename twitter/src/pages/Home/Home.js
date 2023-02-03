de132296fc6eed187431683b7e33415138e94490
Aisana1999 <aisha.asanbaevna.at@gmail.com>
2023-02-03 17:09:51 +0100

    creating containers

---
+++ twitter/src/pages/Home/Home.js
@@ -0,0 +1,11 @@
+import "./Home.css";
+function Home() {
+  return (
+    <div className="home">
+      <div>Left</div>
+      <div>Center</div>
+      <div>Right</div>
+    </div>
+  );
+}
+export default Home;
