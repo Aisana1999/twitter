7abc1fbde316784f7d0f64ead3c461b7f8048a1b
Aisana1999 <aisha.asanbaevna.at@gmail.com>
2023-02-05 21:33:45 +0100

    added  a card elements

--- twitter/src/Components/Home/LoginOption/LoginOption.js
+++ twitter/src/Components/Home/LoginOption/LoginOption.js
@@ -1,4 +1,13 @@
+import "./LoginOption.css";
+
 function LoginOption() {
-  return <h4>Login Option</h4>;
-}
-export default LoginOption;
+  return (
+    <div className="card">
+      <div className="card-body">
+        <h5 className="card-title">New To Twitter ?</h5>
+        <p className="card-text">
+          Sign up now to get your own personalized timeline!
+        </p>
+      
+
+export default LoginOption
