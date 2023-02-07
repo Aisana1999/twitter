868d6761cf374190218bb2db920234acf54c88e4
Aisana1999 <aisha.asanbaevna.at@gmail.com>
2023-02-07 22:05:00 +0100

    created new elements

--- twitter/src/Components/Home/Feed/Feed.js
+++ twitter/src/Components/Home/Feed/Feed.js
@@ -1,4 +1,17 @@
-function Feed() {
-  return <h4>Feed</h4>;
-}
+import Trend from "../Trend/Trend";
+import "./Feed.css";
+import { useState, useEffect } from "react";
+
+function Feed(props) {
+  const [alltweets, setTweets]=useState([]);
+  
+  useEffect(()=>{
+    console.log("new tweets");
+    setTweets(props.tweets);
+  },[props.tweets]);
+
+  // trendData is a variable to store data.
+  // setTrendData is a setter function to update the variable.
+  // useState converts into a state.
+  // state=> any changes will refresh the component.
 export default Feed;
