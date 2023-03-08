a0ef5d0e4e0b827767c4caa3ffae338d875209df
Aisana1999 <aisha.asanbaevna.at@gmail.com>
2023-03-08 22:23:28 +0100

    created a setter function

--- twitter/src/components/Home/Feed/Feed.js
+++ twitter/src/components/Home/Feed/Feed.js
@@ -13,5 +13,25 @@ function Feed(props) {
   // setTrendData is a setter function to update the variable.
   // useState converts into a state.
   // state=> any changes will refresh the component.
+  const [trendData, setTrendData] = useState([]);
+  let data= "My Data";
+  //manages side effects of component re-rendering.
+  useEffect(() => {
+    //task which needs to be managed
+    //1.read data from trend.json file
+    fetch("trend.json");
+    //2.Data is retrieved
+    .then(res=>{
+      console.log("Raw Data");
+      console.log(res);
+      return res.json();
+    })
+    //3.Data is converted .
+    .then(convertedData=>{
+      //trendData=convertedData;
+      setTrendData(convertedData);
+      console.log(trendData.length);//6
+    })
+  });
 }
 export default Feed;
