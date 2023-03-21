8950c2a86cfce9e59baed0ba13b3653c89d1fbd7
Aisana1999 <aisha.asanbaevna.at@gmail.com>
2023-03-21 19:16:13 +0100

    added useState and fetch function

--- twitter/src/components/Home/Feed/Feed.js
+++ twitter/src/components/Home/Feed/Feed.js
@@ -14,14 +14,19 @@ function Feed(props) {
   // useState converts into a state.
   // state=> any changes will refresh the component.
   const [trendData, setTrendData] = useState([]);
+  
   let data= "My Data";
   //manages side effects of component re-rendering.
   useEffect(() => {
     //task which needs to be managed
     //1.read data from trend.json file
-   
+    fetch("trend.json")
     //2.Data is retrieved
-  
+  .then(res=> {
+    console.log("Raw Data");
+    console.log(res);
+    return res.json();
+  })
     //3.Data is converted .
     .then(convertedData=>{
       //trendData=convertedData;
