438357d0be788fb718f285a73dbbffb653b25c04
Aisana1999 <aisha.asanbaevna.at@gmail.com>
2023-03-26 18:55:26 +0200

    created a list

--- twitter/src/components/Home/Logo/Logo.js
+++ twitter/src/components/Home/Logo/Logo.js
@@ -5,6 +5,20 @@ function Logo(props) {
     console.log(nt);
     props.notifyNewTweet(nt);
   }
+   return (
+    <div className="options">
+      <ul>
+        <li>
+        <a href="/">
+          <img src="https://cdn-icons-png.flaticon.com/512/5968/5968823.png"></img>
+        </a>
+        </li>
+        <li className="option">
+          <a href="#">#&nbsp;&nbsp;&nbsp;Explore</a>
+        </li>
+        <li className="option">
+          <a href="#">
+            {" "}
 
   
 
