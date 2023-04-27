277af0f22381862557e9de75761506728bbfddaf
Aisana1999 <aisha.asanbaevna.at@gmail.com>
2023-04-27 20:00:44 +0200

    imported bootstrap elements

--- twitter/src/pages/Home/Home.js
+++ twitter/src/pages/Home/Home.js
@@ -2,6 +2,10 @@ import "./Home.css";
 import Logo from "../../components/Home/Logo/Logo";
 import Feed from "../../components/Home/Feed/Feed";
 import LoginOption from "../../components/Home/LoginOption/LoginOption";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons//font/bootstrap-icons.css";
+import "bootstrap/dist/js/bootstrap.bundle";
+import "bootstrap/dist/js/bootstrap";
 
 function Home() {
   return (
