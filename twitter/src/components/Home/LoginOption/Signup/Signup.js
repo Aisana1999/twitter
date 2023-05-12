4fca33bccc9acc7b7b0cd1ade83945fbbf2335cc
Aisana1999 <aisha.asanbaevna.at@gmail.com>
2023-05-12 20:12:17 +0200

    created a function

--- twitter/src/components/Home/LoginOption/Signup/Signup.js
+++ twitter/src/components/Home/LoginOption/Signup/Signup.js
@@ -1,3 +1,10 @@
+function Signup() {
+  const [user, setUserName] = useState("");
+  const [email, setEmail] = useState("");
+  const [pwd, setPassword] = useState("");
+  const [cpwd, setCpassword] = useState("");
+}
+
 function submit() {
   console.log(user);
   console.log(email);
