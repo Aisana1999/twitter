526474ca2c7e78205f192e2587d2aff0b60dd843
Aisana1999 <aisha.asanbaevna.at@gmail.com>
2023-05-20 17:13:18 +0200

    centered a div

--- twitter/src/pages/Profile/Profile.js
+++ twitter/src/pages/Profile/Profile.js
@@ -2,6 +2,10 @@ import ".Profile.css";
 import Logo from "../../components/Home/Logo/Logo";
 import LoginOption from "../../components/Home/LoginOption/LoginOption";
 function Profile() {
+  <div class="header">
+    <h1> Header</h1>
+  </div>;
+
   return (
     <div className="home">
       <div className="leftContainer">
